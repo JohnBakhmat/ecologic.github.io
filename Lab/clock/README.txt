42f1fd5d678f8eef50fcd3227113d8019911e898
MrDiviner <yevhenii.bakhmat@nure.ua>
2020-05-28 11:06:16 +0300

    LabFIx

---
+++ Lab/clock/README.txt
@@ -0,0 +1,6 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/ky0suke/pen/MyNXWX.
+
+ A pretty clock made with html, css and some javascript to get the time and update the clock.
+
+Inspired by Zhu Sheng's work on dribble, check his work :
+https://dribbble.com/shots/2720228-Clock-App
---
+++ Lab/lab4/action.js
@@ -0,0 +1,51 @@
+loadXMLDoc();
+function loadXMLDoc(){
+    const xhttp = new XMLHttpRequest ();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            myFunction(this)
+        }
+    };
+    xhttp.open("GET", "lab4/Books.xml", true);
+    xhttp.send();}
+function myFunction(xml) {
+    var i;
+    var xmlDoc = xml.responseXML;
+    var table="<tr><th>Title</th><th>Author</th><th>Category</th><th>Year</th><th>Price</th></tr>";
+    var x = xmlDoc.getElementsByTagName("book");
+    for (i = 0; i <x.length; i++) {
+        table += "<tr><td>" +
+            x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue +
+            "</td><td>" +
+            x[i].getElementsByTagName("author")[0].childNodes[0].nodeValue +
+            "</td><td>" +
+            x[i].getElementsByTagName("category")[0].childNodes[0].nodeValue +
+            "</td><td>" +
+            x[i].getElementsByTagName("year")[0].childNodes[0].nodeValue +
+            "</td><td>" +
+            x[i].getElementsByTagName("price")[0].childNodes[0].nodeValue +
+            "</td></tr>";
+    }
+    document.getElementById("demo").innerHTML = table;
+}
+function search() {
+    // Declare variables
+    let input, filter, table, tr, td,td2, i, txtValue;
+    input = document.getElementById("Search");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("demo");
+    tr = table.getElementsByTagName("tr");
+
+    for ( i = 0 ; i < tr.length ; i ++ ) {
+        td = tr[ i ].getElementsByTagName ("td")[ 0 ];
+        td2 = tr[ i ].getElementsByTagName ("td")[ 0 ];
+        if ( td ) {
+            txtValue = td.textContent || td.innerText;
+            if ( txtValue.toUpperCase ().indexOf (filter) > - 1 ) {
+                tr[ i ].style.display = "";
+            } else {
+                tr[ i ].style.display = "none";
+            }
+        }
+    }
+}
