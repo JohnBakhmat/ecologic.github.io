3f09e92c8e785215e814a633be1a52a4db958599
MrDiviner <yevhenii.bakhmat@nure.ua>
2020-05-28 10:51:47 +0300

    Booom

---
+++ Main/redirect.js
@@ -0,0 +1,33 @@
+function toChloropyta () {
+    window.location = "../Pages/Chloropyta.html";
+}
+function toEmbryophyta () {
+    window.location = "../Pages/Embryophyta.html";
+}
+function toEquisetum () {
+    window.location = "../Pages/Equisetum.html";
+}
+function toFilicopsida () {
+    window.location = "../Pages/Filicopsida.html";
+}
+function toLycopodium () {
+    window.location = "../Pages/Lycopodium.html";
+}
+function toBryophyta () {
+    window.location = "../Pages/Bryophyta.html";
+}
+function toMain () {
+    window.location = "../Main/index.html";
+}
+function toGitHub () {
+    window.location = "https://github.com/MrDiviner/Project-H"
+}
+function toTelegram () {
+    window.location = "https://t.me/joinchat/AAAAAE71KQXzG_RhAfoitA"
+}
+function toTwitter () {
+    window.location = "https://twitter.com/ECOLogi74433644"
+}
+function toLab2(){
+	window.location = "../Lab/index.html";
+}
